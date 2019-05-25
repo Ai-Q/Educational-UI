57794bf106a41ac5e9b24bee268cd02344decc89
Administrator <862821775@qq.com>
2019-05-25 22:32:26 +0800

    初版

---
+++ src/components/Login.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="row">
+      <div class="col-lg-6">左</div>
+      <div class="col-lg-6">右</div>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'Login'
+  }
+</script>
+
+<style scoped>
+
+</style>
